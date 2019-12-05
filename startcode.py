6eea79717251d2ed9307c22d54d87d9bb0311f37
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2019-12-05 00:35:55 -0500

    Update startcode.py

--- startcode.py
+++ startcode.py
@@ -15,12 +15,24 @@ if len(sys.argv)!=2:                  ## Check for error in usage syntax
 
 else:
     img = cv2.imread(sys.argv[1],cv2.IMREAD_COLOR)  ## Read image file
-
-    if (img.any() == None):                      ## Check for invalid input
-        print("Could not open or find the image")
+    
+    try:
+        if (img.any() == None):                      ## Check for invalid input
+            print("Could not open or find the image")
+    except AttributeError:
+        if (img == None):                      ## Check for invalid input
+            print("Could not open or find the image")
+        else:
+            cv2.namedWindow('Display Window')        ## create window for display
+            cv2.imshow('Display Window',img)         ## Show image in the window
+            print ("size of image: ",img.shape)        ## print size of image
+            cv2.waitKey(0)                           ## Wait for keystroke
+            cv2.destroyAllWindows()
     else:
-        cv2.namedWindow('Display Window')        ## create window for display
-        cv2.imshow('Display Window',img)         ## Show image in the window
-        print ("size of image: ",img.shape)        ## print size of image
-        cv2.waitKey(0)                           ## Wait for keystroke
-        cv2.destroyAllWindows()                  ## Destroy all windows
+        if (img.any() == None):                      ## Check for invalid input
+            print("Could not open or find the image")
+        else:
+            cv2.namedWindow('Display Window')        ## create window for display
+            cv2.imshow('Display Window',img)         ## Show image in the window        ## print size of image
+            cv2.waitKey(0)                           ## Wait for keystroke
+            cv2.destroyAllWindows()                  ## Destroy all windows
