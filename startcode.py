f6dac5f89d0f7fe853613267de448b06300c5e00
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2019-12-05 00:41:24 -0500

    Update startcode.py

--- startcode.py
+++ startcode.py
@@ -1,14 +1,10 @@
-''' filename : display_image.py
-Description : This sample demonstrates how to read an image, display it on the window and print image size.
-This is Python version of this tutorial : http://opencv.itseez.com/doc/tutorials/introduction/display_image/display_image.html#display-image
-Level : Beginner
-Benefits : Learn to load image and display it in window
-Usage : python display_image.py <image_file> 
-Written by : Abid K. (abidrahman2@gmail.com) , Visit opencvpython.blogspot.com for more tutorials '''
 
 import cv2
 import sys
 import numpy as np
+import ee
+
+ee.Initialize()
 
 if len(sys.argv)!=2:                  ## Check for error in usage syntax
     print("Usage : python display_image.py <image_file>")
