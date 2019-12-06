f516c28539354f5c03bb44f02a253a0bd5ba882f
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2019-12-05 22:52:09 -0500

    Update startcode.py

--- startcode.py
+++ startcode.py
@@ -19,7 +19,7 @@ products = api.query(footprint,
                     )
 
 
-products.items()
+print(products.items())
     
 #All of this is just printing an image
 if len(sys.argv)!=2:                  ## Check for error in usage syntax
