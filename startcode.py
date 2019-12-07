dee3fc23d3f9e94b66a5d52e1d18e2ef0a4ea5fe
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2019-12-07 18:06:18 -0500

    Update startcode.py

--- startcode.py
+++ startcode.py
@@ -14,9 +14,7 @@ from shapely.geometry import MultiPolygon, Polygon
 """alpha = 'teamairo' 
 beta = 'a1r0-2016' 
 api = SentinelAPI(alpha, beta, 'https://scihub.copernicus.eu/dhus')
-
 #Gets a longitude and latitude from the user
-
 footprint = geojson_to_wkt(read_geojson('swiss_zip.geojson'))
     
 products = api.query(footprint,
@@ -25,27 +23,20 @@ products = api.query(footprint,
                      processinglevel = 'Level-2A',
                      cloudcoverpercentage = (0,10)
                     )
-
-
 product = list(products.keys())[0]
 api.download(product)
-
 #this turns the list of satellite images into a database and takes the title of the first one so the zipfile can be recognized
 products_df = api.to_dataframe(products)
 title = products_df['title'].iloc[0]
-
 #extracts everything from the zipfile downloaded
 with ZipFile(title + '.zip', 'r') as zipObj:
    zipObj.extractall()
-
 #Finding all the files that we can combine into an image
 list = glob.glob('**/*.jp2', recursive=True)
-
 b2 = rio.open(list[1])
 b3 = rio.open(list[2])
 b4 = rio.open(list[3])
 print(list[2])
-
 with rio.open('RGB.tiff','w',driver='Gtiff', width=b4.width, height=b4.height, 
               count=3,crs=b4.crs,transform=b4.transform, dtype=b4.dtypes[0]) as rgb:
     rgb.write(b2.read(1),1) 
@@ -84,7 +75,7 @@ for (lower, upper) in boundaries:
 	cv2.waitKey(0)
 	
 boundaries2 = [
-	([5, 64, 37], [66,161, 117])
+	([32, 0, 4], [184,104, 112])
 ]
 
 for (lower, upper) in boundaries2:
@@ -102,25 +93,6 @@ for (lower, upper) in boundaries2:
 	cv2.imshow("images", np.hstack([image, output]))
 	cv2.waitKey(0)
 	
-boundaries3 = [
-	([5, 64, 37], [128, 255, 196])
-]
-
-for (lower, upper) in boundaries3:
-	# create NumPy arrays from the boundaries
-	lower3 = np.array(lower, dtype = "uint8")
-	upper3 = np.array(upper, dtype = "uint8")
- 
-	# find the colors within the specified boundaries and apply
-	# the mask
-	mask3 = cv2.inRange(image, lower3, upper3)
-	output3 = cv2.bitwise_and(image, image, mask = mask3)
-        
-	
-	# show the images
-	cv2.imshow("images", np.hstack([image, output]))
-	cv2.waitKey(0)
-	
 nPixels = np.count_nonzero(mask == 0)
 count = mask.size
 percentage = nPixels/count
@@ -129,19 +101,9 @@ nPixels2 = np.count_nonzero(mask2 == 0)
 count2 = mask2.size
 percentage2 = nPixels2/count2
 
-nPixels3 = np.count_nonzero(mask3 == 0)
-count3 = mask3.size
-percentage3 = nPixels3/count3
 
-percentage = 100 - percentage
-percentage2 = 100 - percentage2
-percentage3 = 100 - percentage3
-if percentage > 0.1 and percentage2 < 0.8 and percentage3 > 0.03:
+if percentage < 0.9 and percentage2 < 0.6:
 	print("This is suitable for trees")
 	
 print(percentage)
 print(percentage2)
-print(percentage3)
-		
-	
-	
