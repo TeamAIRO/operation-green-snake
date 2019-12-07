0df8798c65af1c4f5bf748184dcb03544b4926ab
TeamAIRO <45614926+TeamAIRO@users.noreply.github.com>
2019-12-07 01:10:51 -0500

    Update startcode.py

--- startcode.py
+++ startcode.py
@@ -53,29 +53,25 @@ with rio.open('RGB.tiff','w',driver='Gtiff', width=b4.width, height=b4.height,
     rgb.close()
     
 #All of this is just printing an image
-if len(sys.argv)!=2:                  ## Check for error in usage syntax
-    print("Usage : python display_image.py <image_file>")
-
-else:
-    img = cv2.imread(sys.argv[1],cv2.IMREAD_COLOR)  ## Read image file
+img = cv2.imread('RGB.tiff',cv2.IMREAD_COLOR)  ## Read image file
     
-    try:
-        if (img.any() == None):                      ## Check for invalid input
-            print("Could not open or find the image")
-    except AttributeError:
-        if (img == None):                      ## Check for invalid input
-            print("Could not open or find the image")
-        else:
-            cv2.namedWindow('Display Window')        ## create window for display
-            cv2.imshow('Display Window',img)         ## Show image in the window
-            print ("size of image: ",img.shape)        ## print size of image
-            cv2.waitKey(0)                           ## Wait for keystroke
-            cv2.destroyAllWindows()
+try:
+    if (img.any() == None):                      ## Check for invalid input
+        print("Could not open or find the image")
+except AttributeError:
+    if (img == None):                      ## Check for invalid input
+        print("Could not open or find the image")
     else:
-        if (img.any() == None):                      ## Check for invalid input
-            print("Could not open or find the image")
-        else:
-            cv2.namedWindow('Display Window')        ## create window for display
-            cv2.imshow('Display Window',img)         ## Show image in the window        ## print size of image
-            cv2.waitKey(0)                           ## Wait for keystroke
-            cv2.destroyAllWindows()                  ## Destroy all windows
+        cv2.namedWindow('Display Window')        ## create window for display
+        cv2.imshow('Display Window',img)         ## Show image in the window
+        print ("size of image: ",img.shape)        ## print size of image
+        cv2.waitKey(0)                           ## Wait for keystroke
+        cv2.destroyAllWindows()
+else:
+    if (img.any() == None):                      ## Check for invalid input
+        print("Could not open or find the image")
+    else:
+        cv2.namedWindow('Display Window')        ## create window for display
+        cv2.imshow('Display Window',img)         ## Show image in the window        ## print size of image
+        cv2.waitKey(0)                           ## Wait for keystroke
+        cv2.destroyAllWindows()                  ## Destroy all windows
